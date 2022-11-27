f58859638d4be73227d6341815743058550fa4d3
Popov Nikita <58226087+Inoskeee@users.noreply.github.com>
2022-11-28 00:59:20 +0500

    Delete settings.py

--- settings.py
+++
@@ -1,4 +0,0 @@
-TOKEN = 'Nzc4NDkzMDIxOTg4ODQ3NjQ3.X7SyIA.HfOxcbMAkYBBb7sRMmT4yajaYck'
-
-MONGODB_LINK = "mongodb+srv://admin:123Rfhnjy456@botanton.mvodf.mongodb.net/anton_bot?retryWrites=true&w=majority"
-MONGO_DB = "anton_bot"
